5e6df98d030a482da7e01fb9b9e5c82356966aff
libracoder <libracoder@gmail.com>
2021-01-22 06:05:18 +0100

    test: added test to initialize rover
    it test for, when the rover dosent move

--- index.test.js
+++ index.test.js
@@ -3,5 +3,14 @@ const {expect} = require("chai");
 const Rover = require("./index");
 
 describe("marsrover", function () {
+    let rover;
+    beforeEach(function () {
+        rover = new Rover();
+    });
+    it("tests for no movement", function () {
+        expect(rover.currentXPos).to.be.equal(4)
+        expect(rover.currentYPos).to.be.equal(2)
+        expect(rover.direction).to.be.equal("EAST")
+    });
 
 });
